7a830310c8d5fbf988ed8f1c4425431cd6d04ddb
pepijnTEW <37923@ma-web.nl>
2024-12-07 12:56:40 +0100

    einde les 1

---
+++ m6prog_project/public/index.php
@@ -0,0 +1,15 @@
+<?php
+require_once '../source/config.php';
+require_once SOURCE_ROOT . 'database.php';
+$connection = database_connect();
+$sql = "SELECT * FROM weersomstandighedenPerDag WHERE plaats IN (?,?) ORDER BY datum"; 
+$stmt = $connection->prepare($sql);
+$plaats = 'rotterdam';
+$plaats2 = 'amsterdam';
+$stmt->bind_param('ss', $plaats, $plaats2);
+$stmt->execute();
+$result = $stmt->get_result();
+while(
+    $weersomstandigheden = mysqli_fetch_assoc($result)){
+    var_dump( $weersomstandigheden );
+}
