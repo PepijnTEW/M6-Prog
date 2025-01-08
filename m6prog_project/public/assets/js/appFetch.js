c9e32bda8490137536dbb93e067a1f13d085168b
pepijnTEW <37923@ma-web.nl>
2025-01-08 11:28:23 +0100

    needed push

--- m6prog_project/public/assets/js/appFetch.js
+++ m6prog_project/public/assets/js/appFetch.js
@@ -1,10 +1,16 @@
 "use strict";
 let getForm = document.getElementById('getForm');
+let postForm = document.getElementById('postForm');
 
 getForm.addEventListener("submit", (event) => {
     event.preventDefault();
     toPhpWithGet(event);
 });
+
+postForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    toPhpWithPost(event);
+})
 
 function toPhpWithGet() {
     let form = event.target;
@@ -19,4 +25,15 @@ function toPhpWithGet() {
             console.log(response);
         });
 }
-
+function toPhpWithPost() {
+    let form = event.target;
+    let jsonform = FromToDictionary(form);
+    console.log(jsonform);
+}
+function FromToDictionary(form) {
+    const data = new FormData(form);
+    let formKeyValue = {};
+    for (const [name, value] of data) {
+        formKeyValue[name] = value;
+    } return formKeyValue;
+}
